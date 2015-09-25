f5dd6e3a50e9285d06849c8466c456a04edc3c0a
JuliusGJimenez0522 <jjimenez149@student.rcc.edu>
2015-09-25 15:15:42 -0700

    Inventory Manager COMPLETED
    We merged the inventory manager. #swag

---
+++ MinecraftTeam3/Assets/InventoryManager.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InventoryManager : MonoBehaviour 
+{
+	public int woodInventory = 0;
+	public int stoneInventory = 0;
+	public int dirtInventory = 0;
+	public int sandInventory = 0;
+
+	public int porkFood = 0;
+	public int chickenFood = 0;
+
+	public Text woodInvText;
+	public Text stoneInvText;
+	public Text dirtInvText;
+	public Text sandInvText;
+
+	public Text porkInvText;
+	public Text chickenInvText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetCountText ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		if(Input.GetKeyDown(KeyCode.Q))
+//		{
+//			Debug.Log("Q was Pressed");
+//			woodInventory = woodInventory + 1;
+//			Debug.Log("woodInventory = " + woodInventory);
+//			SetCountText ();
+//		}
+//		if(Input.GetKeyDown(KeyCode.W))
+//		{
+//			Debug.Log("W was Pressed");
+//			stoneInventory = stoneInventory + 1;
+//			Debug.Log("stoneInventory = " + stoneInventory);
+//			SetCountText ();
+//		}
+//		if(Input.GetKeyDown(KeyCode.E))
+//		{
+//			Debug.Log("E was Pressed");
+//			dirtInventory = dirtInventory + 1;
+//			Debug.Log("dirtInventory = " + dirtInventory);
+//			SetCountText ();
+//		}
+//		if(Input.GetKeyDown(KeyCode.R))
+//		{
+//			Debug.Log("R was Pressed");
+//			sandInventory = sandInventory + 1;
+//			Debug.Log("sandInventory = " + sandInventory);
+//			SetCountText ();
+//		}
+//		if(Input.GetKeyDown(KeyCode.T))
+//		{
+//			Debug.Log("T was Pressed");
+//			porkFood = porkFood + 1;
+//			Debug.Log("porkFood = " + porkFood);
+//			SetCountText ();
+//		}
+//		if(Input.GetKeyDown(KeyCode.Y))
+//		{
+//			Debug.Log("Y was Pressed");
+//			chickenFood = chickenFood + 1;
+//			Debug.Log("chickenFood = " + chickenFood);
+//			SetCountText ();
+//		}
+	}
+	void SetCountText ()
+	{
+		woodInvText.text = "Wood: " + woodInventory.ToString ();
+		stoneInvText.text = "Stone: " + stoneInventory.ToString ();
+		dirtInvText.text = "Dirt: " + dirtInventory.ToString ();
+		sandInvText.text = "Sand: " + sandInventory.ToString ();
+
+		porkInvText.text = "Bacon: " + porkFood.ToString ();
+		chickenInvText.text = "Chicken: " + chickenFood.ToString ();
+
+	}
+
+	public void SetDirt()
+	{
+		dirtInventory = dirtInventory + 1;
+		SetCountText ();
+	}
+	public void SetWood()
+	{
+		woodInventory = woodInventory + 1;
+		SetCountText ();
+	}
+	public void SetSand()
+	{
+		sandInventory = sandInventory + 1;
+		SetCountText ();
+	}
+	public void SetStone()
+	{
+		stoneInventory = stoneInventory + 1;
+		SetCountText ();
+	}
+	public void SetPork()
+	{
+		porkFood = porkFood + 1;
+		SetCountText ();
+	}
+	public void SetChicken()
+	{
+		chickenFood = chickenFood + 1;
+		SetCountText ();
+	}
+}
---
+++ MinecraftTeam3/Assets/TestPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour 
+{
+	public GameObject gameManager;
+
+	public int dirtAmount;
+	public int woodAmount;
+	public int stoneAmount;
+	public int sandAmount;
+	public int porkAmount;
+	public int chickenAmount;
+
+	InventoryManager inventoryManager;
+	
+	void Start () 
+	{
+		inventoryManager = gameManager.GetComponent<InventoryManager> ();
+	}
+
+	void Update () 
+	{
+		dirtAmount = inventoryManager.dirtInventory;
+		woodAmount = inventoryManager.woodInventory;
+		stoneAmount = inventoryManager.stoneInventory;
+		sandAmount = inventoryManager.sandInventory;
+		porkAmount = inventoryManager.porkFood;
+		chickenAmount = inventoryManager.chickenFood;
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "dirt") 
+		{
+			inventoryManager.SetDirt();
+		}
+		if (other.gameObject.tag == "wood") 
+		{
+			inventoryManager.SetWood();
+		}
+		if (other.gameObject.tag == "stone") 
+		{
+			inventoryManager.SetStone();
+		}
+		if (other.gameObject.tag == "sand") 
+		{
+			inventoryManager.SetSand();
+		}
+		if (other.gameObject.tag == "pork") 
+		{
+			inventoryManager.SetPork();
+		}
+		if (other.gameObject.tag == "chicken") 
+		{
+			inventoryManager.SetChicken();
+		}
+	}
+}
