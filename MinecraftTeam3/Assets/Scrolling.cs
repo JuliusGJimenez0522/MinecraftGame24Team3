425f061e50a1caf2335bd649b72e5258115dd2ae
GRRRT <gnordin@student.rcc.edu>
2015-09-25 14:26:40 -0700

    Added a scroll wheel thing
    no really, all I did was add a scroll wheel thing. However, no hotbar
    exists to tie it with yet...

---
+++ MinecraftTeam3/Assets/Scrolling.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling : MonoBehaviour 
+{
+	// I wonder if I can get it to just scroll up and down between 1 and 9, which would allow us to generate if statement conditions when slots equals a specific number...
+	public float slots = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		slots = Input.GetAxis("Mouse ScrollWheel");
+		if (slots > 0f)
+		{
+			// scroll up
+			print ("up");
+		}
+		else if (slots < 0f)
+		{
+			// scroll down
+			print ("down");
+		}
+
+
+	}
+}
